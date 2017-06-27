efccaf31c8289686f5c2d33abbca4f284fb60951
Prithvi Velpuri <pvelpur@purdue.edu>
2017-06-27 10:30:25 -0400

    Adds the name to a paragraph in a div section called stats

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 function changeHeading(ev){
+    
+}
+const p = document.querySelector('#stats p')
+p.style.display = 'none'
+
+function changeParagraph(ev){
     ev.preventDefault()
     const f = ev.target
+    p.style.display = ''
     const name = f.personName.value
     const heading = document.querySelector('h1')
-    heading.textContent = name
-
+    p.textContent += name
+    
 }
 /*const btn = document.querySelector('button')
 btn.addEventListener('click', changeHeading)
@@ -13,5 +20,5 @@ btn.addEventListener('click', changeHeading)
 const p = document.querySelector('#instructions')
 p.addEventListener('click', changeHeading)*/
 
-const personForm = document.querySelector("#person-form")
-personForm.addEventListener('submit', changeHeading)
+const personForm = document.querySelector("#person-form1")
+personForm.addEventListener('submit', changeParagraph)
